e01800406115032ea73e8c72072346985ad01276
mstefanec <mstefanec1@etfos.hr>
2020-12-18 11:56:37 +0100

    task 1

---
+++ lv2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace lv2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
